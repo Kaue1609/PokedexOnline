21890afc765e162b9c7a280a5cbe187ca3afca7a
alisson <gui340493@gmail.com>
2023-06-28 19:50:55 -0300

    construção dos sites individuais

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
 function oi() {
     alert("Muito obrigado por visitar o nosso site! <3")
 }
+
+let url = "https://pokeapi.co/api/v2/pokemon/bulbasaur"
+fetch(url)
+.then(response => response.json())
+.then(response => console.log(response))
+
+function exibir(types) {
+    console.log(types)
+    let types = types[0].type
+    console.log(type)
+}
+function seetypes(){
+}
