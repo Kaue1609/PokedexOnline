8fcfab9c2ffbb9784faaabf42233ba4e7a1d118a
Kauê Martins <kauemm16@gmail.com>
2023-06-28 23:01:26 -0300

    link de botão

--- index.js
+++ index.js
@@ -1,3 +1,37 @@
 function oi() {
     alert("Muito obrigado por visitar o nosso site! <3")
 }
+
+
+/*let tipos = 'https://pokeapi.co/api/v2/pokemon/bulbasaur'
+const types = fetch(tipos)
+.then(response => response.json())
+.then(response => exibirTipos(response.types))
+
+function exibirTipos(tipos) {
+    let typebox = document.getElementsByClassName("typebox")
+    for (let i = 0; i < tipos.length; i++) {
+
+        const tipoEl = document.createElement('p')
+        tipoEl.innerText = tipos[i].type.name
+        typebox.append(tipoEl)
+    }
+} */
+
+let tipos = 'https://pokeapi.co/api/v2/pokemon/bulbasaur'
+const types = fetch(tipos)
+    .then(response => response.json())
+    .then(response => exibirTipos(response.types))
+
+function exibirTipos(tipos) {
+    for (let i = 0; i < tipos.length; i++) {
+
+        const tipoEl = document.createElement('p')
+
+        tipoEl.innerText = tipos[i].type.name
+
+        document.body.appendChild(tipoEl)
+
+    }
+
+}
